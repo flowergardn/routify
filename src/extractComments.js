f874f08cb33091b80920921fe6cdeaf684e4a457
Cryogenetics <83240673+Cryogenetics@users.noreply.github.com>
2022-08-02 13:58:42 -0500

    Feat: Clean up code for readability, add params and responses by status response

--- src/extractComments.js
+++ src/extractComments.js
@@ -1,24 +1,42 @@
 const paramRegex = /@(\w+)\s*: .*/gim;
 
 // Used to remove whitespace from the start of a string
-function ltrim(str) {
+function trimLeft(str) {
   if (!str) return str;
   return str.replace(/^\s+/g, "");
 }
 
-// Used for when the annotation value is expected to be a string, not JSON.
-const getValue = (match) => match.split(":")[1].replace(/\s/g, "");
-// Used for when the annotation value is expected to contain a colon (JSON)
-const getValue2 = (match) => {
-  const split = match.split(":");
-  split.shift();
-  return ltrim(split.join(":"));
-};
+
+const parseMatchByExpectedType = (match, expectedType) => {
+  switch (expectedType) {
+    // Used for when the annotation value is expected to be a string, not JSON.
+    case "string":
+      return match.split(":")[1].replace(/\s/g, "")
+    // Used for when the annotation value is expected to contain a colon (JSON)
+    case "json":
+      const split = match.split(":")
+        split.shift()
+        return trimLeft(split.join(":"))
+  }
+
+}
+
+
+const routeFormat = {
+  path: "",
+  method: "",
+  description: "",
+  body: undefined,
+  returns: "",
+  params: undefined,
+  responses: {},
+}
+
 
 function parseFile(contents) {
   const routes = {};
 
-  let currentRoute = {};
+  let currentRoute = {...routeFormat};
   let linesProcessed = 1;
 
   function checkLine(line) {
@@ -33,7 +51,7 @@ function parseFile(contents) {
 
       if (annotation.includes("@route") && currentRoute.path) {
         routes[currentRoute.path] = currentRoute;
-        currentRoute = {};
+        currentRoute = {...routeFormat};;
       }
 
       if (annotation.includes("@route")) {
@@ -42,21 +60,31 @@ function parseFile(contents) {
         desc = desc.replace("\r", "");
         currentRoute.description = desc;
 
-        currentRoute.path = getValue2(annotation);
+        currentRoute.path = parseMatchByExpectedType(annotation, "json");
         routes[currentRoute.path] = {};
       } else {
         // Get the method
         if (annotation.includes("@method"))
-          currentRoute.method = getValue(annotation);
+          currentRoute.method = parseMatchByExpectedType(annotation, "string");
         // Get the data that it requires
         if (annotation.includes("@body"))
-          currentRoute.body = getValue2(annotation);
+          currentRoute.body = parseMatchByExpectedType(annotation, "json");
         // Get the data that it returns
         if (annotation.includes("@returns"))
-          currentRoute.returns = getValue2(annotation);
+          currentRoute.returns = parseMatchByExpectedType(annotation, "json");
         // Get the headers that it requires
         if (annotation.includes("@headers"))
-          currentRoute.headers = getValue2(annotation);
+          currentRoute.headers = parseMatchByExpectedType(annotation, "json");
+        // Get responses by status code
+        if (annotation.match(/@\d{3}/)) {
+            const statusCode = annotation.match(/@(\d{3})/)[1];
+            console.log(statusCode)
+            console.log(currentRoute)
+            currentRoute.responses[statusCode] = parseMatchByExpectedType(annotation, "json");
+        }
+        // Get the params that it requires
+        if (annotation.includes("@params"))
+            currentRoute.params = parseMatchByExpectedType(annotation, "json");
       }
     }
 
